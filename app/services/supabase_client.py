e54b73d6a23c980c66354bed3b35fbef16f53187
Tatiana Brimm <tbrimm1@student.gsu.edu>
2025-12-10 06:53:51 -0500

    Updated Supabase mock client , so GH doesn't touch supabase driectly during testing

--- app/services/supabase_client.py
+++ app/services/supabase_client.py
@@ -1,12 +1,15 @@
-from supabase import create_client, Client
-from dotenv import load_dotenv
 import os
+from supabase import create_client, Client
 
+USE_FAKE_SUPABASE = os.getenv("USE_FAKE_SUPABASE", "false").lower() == "true"
 
-load_dotenv()
+if USE_FAKE_SUPABASE:
+    from app.services.supabase_client_mock import supabase
+else:
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
+    if not SUPABASE_URL or not SUPABASE_KEY:
+        raise ValueError("Supabase credentials missing.")
 
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
